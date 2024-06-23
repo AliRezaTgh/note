818c9468a38503c53b8f450091ef42debe7a268e
AliReza <46268239+AliRezaTgh@users.noreply.github.com>
2024-06-23 18:44:21 +0330

    note

---
+++ frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import react from "react"
+import { useState } from "react"
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import NotFound from "./pages/NotFound"
+import Home from "./pages/Home"
+import ProtectedRoute from "./components/ProtectedRoutes"
+
+function Logout () {
+  localStorage.clear()
+  return <Navigate to="/login"/>
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register/>
+}
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/Login" element={<Login />}/>
+        <Route path="/Logout" element={<Logout />}/>
+        <Route path="/Register" element={<RegisterAndLogout />}/>
+        <Route path="*" element={<NotFound />}/>                        
+      </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+function NotFound() {
+    return <div>
+        <h1>404 oops the url is wrong </h1>
+        <h2>Page not Found</h2>
+    </div>;
+}
+
+export default NotFound;
